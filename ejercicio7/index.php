d1e13e3802a1eaa439024e62a837404540e732b6
LAPTOP-NND7KTNQ\JUAN LOPEZ <charlie.lopez@epn.edu.ec>
2019-04-30 12:44:22 -0500

    Ejercicio 7 Agregado

--- ejercicio7/index.php
+++ ejercicio7/index.php
@@ -2,5 +2,28 @@
 /*
 Escribir código PHP que inicialice un arreglo con los números del 1 al 365. Determine a que mes pertenece el día del año. Y luego calcule cuántos días faltan para que se acabe el año con cada día del año, y muestre el mensaje “Falta poco para el año nuevo” solo si faltan menos de 5 días.
 */
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+	<div class = "container">
+        <h1 class = 'page-header'>MES</h1>
+        <form action = "Mes.php" method = "POST"> 
+            <div class = "form-group">
+                <label>Ingrese un numero perteneciente al mes:</label>
+                <input type = "text" name = "numero" class = "form-control"/>
+            </div>
+            <div class = "form-group">
+               
+               <button class="btn btn-primary" type="submit">Enviar</button>
+
+            </div>
+        </form>
+    </div>
+
+
+</body>
+</html>
